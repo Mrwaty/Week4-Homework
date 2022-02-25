8c40ee327302722f6da11561888766a3d8a96a6b
Mrwaty <98746981+Mrwaty@users.noreply.github.com>
2022-02-26 00:35:50 +0100

    Update hw3_Requests.py

--- MERVE ATAY WEEK4_Homework/hw3_Requests.py
+++ MERVE ATAY WEEK4_Homework/hw3_Requests.py
@@ -24,3 +24,19 @@ print("Lengths of characters in the list: " , len_list)
 for j in range(10):  
     if max(len_list)== len_list[j]:
         print("'" , my_list[j] , "'", " has the most characters!")
+
+"""
+General Information:
+I want to choose random information with the requests module.
+
+Acceptance Criteria
+* Get the name, surname, city and country information of a random person by using the requests module.
+(Example=> name:Amber, surname: Murray, city: Salisbury, country: United Kingdom)
+* Make lowercase and adjacent by removing the spaces.
+(Example=> ambermurraysalisburyunitedkingdom)
+* Then randomly shuffle the location of all the characters.
+(Example=> mberarrumasyarubsiluniydetmdoingk)
+* Apply this process for 100 different people and write in a list.
+* Find that has the most characters and print it.
+
+"""
